e7195af9bbfc4d30f0d561a317a0bddb40ef5211
songle <1964584185@qq.com>
2023-02-26 16:47:57 +0800

    Initial commit

---
+++ src/main/java/com/util4j/Util4jApplication.java
@@ -0,0 +1,20 @@
+package com.util4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ */
+@SpringBootApplication
+public class Util4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Util4jApplication.class, args);
+    }
+
+}
